08d14cee5cea3ba00b798ec3b8ad3678184fb2d1
JaneJung-K <piaojane@daum.net>
2021-04-19 18:29:27 +0900

    first commit

---
+++ TimeCapture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeCapture
+//
+//  Created by 렌트닷컴 on 2021/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
